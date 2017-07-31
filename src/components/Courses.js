446bcc07e03d378108b9c9a458e491d32253ab6f
Hoang Anh Vo <vohoanganhkhtn@gmail.com>
2017-07-31 19:10:32 +0300

    Using math object

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -6,23 +6,23 @@ import CSS from './courses/CSS'
 import HTML from './courses/HTML'
 
 
-const Courses = () => (
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
-        <li><NavLink  to='/courses/html'>HTML</NavLink></li>
-        <li><NavLink  to='/courses/css'>CSS</NavLink></li>
-        <li><NavLink  to='/courses/javascript'>JavaScript</NavLink></li>
+        <li><NavLink  to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink  to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink  to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
 
     {/* Write routes here... */}
 
-    <Route exact path='/courses' render={()=> <Redirect to='/courses/html'/>} ></Route>
-    <Route path='/courses/html' component={HTML}></Route>
-    <Route path='/courses/javascript' component={JavaScript}></Route>
-    <Route path='/courses/css' component={CSS}></Route>
+    <Route exact path={`${match.path}`} render={()=> <Redirect to='/courses/html'/>} ></Route>
+    <Route path={`${match.path}/html`} component={HTML}></Route>
+    <Route path={`${match.path}/javascript`} component={JavaScript}></Route>
+    <Route path={`${match.path}/css`} component={CSS}></Route>
   </div>
 );
 
